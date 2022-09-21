21808138a90fcdcd6f4e7574ee38aaf1f885077a
Nicky Wrightson <nickywrightson@Nickys-MBP.localdomain>
2022-09-21 10:32:52 +0100

    Initial version of mac setup files

---
+++ Brewfile
@@ -0,0 +1,66 @@
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+cask_args appdir: "~/Applications"
+
+# Security
+# Rosetta is needed for LastPas on Silicon chips
+cask "lastpass"
+brew "lastpass-cli"
+
+# Source control
+brew "git"
+
+# Programming Languages
+brew "go"
+brew "python"
+brew "pyenv"
+
+# Docker & Containers
+brew "docker" 
+brew "docker-compose"
+# whalebrew "whalebrew/wget"
+
+# Infra
+brew "terraform"
+
+#  Browsers
+cask "google-chrome"
+cask "opera"
+
+# Productivity/Comms
+cask "slack"
+cask "zoom"
+cask "google-drive"
+cask "grammarly"
+
+# Text Editors & IDEs
+cask "sublime-text"
+cask "visual-studio-code"
+
+# Terminal(s)
+cask "iterm2"
+brew "zsh"
+brew "zsh-syntax-highlighting"
+
+# For Oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# CLIs
+cask "google-cloud-sdk"
+
+# Data
+# cask "tableplus"
+# cask "anaconda"
+
+# Images
+cask "gimp"
+
+# General
+# ToDo: Add setup for mackup
+brew "mackup"
+
+# Apple Store apps
+# Not working at the moment - I suspect it is to do with payment options for the apple id
+#mas "Lightshot Screenshot", id: 526298438
+
---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+echo "Welcome to your mac setup \n"
+
+
+#########################################
+# HOMEBREW INSTALLATION                 #
+#########################################
+
+if ! command -v brew &> /dev/null; then 
+	echo "Installing Homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	printf 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
+    export PATH="/usr/local/bin:$PATH"
+	brew tap Homebrew/bundle
+else
+        echo "Homebrew already installed."
+fi
+
+#########################################
+# APPLICATION INSTALLATION VIA BREWFILE #
+#########################################
+
+echo "Installing applications via the Brewfile"
+brew bundle --file ~/Code/mac-setup/Brewfile
+
+
+#########################################
+# oh-my-zsh                             #
+#########################################
+
+if [[ ! -d ~/.oh-my-zsh ]]; then
+    echo "Installing on-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo "oh-my-zsh already installed."
+fi
+
+#########################################
+# Mackup and dotfile config             #
+#########################################
+
+##############################################
+# Private & Public Key setup via laspass cli #
+##############################################
+if [[ ! -e ~/.ssh ]]; then
+    # TODO: Add to Lastpass via the CLI - Password not working though
+    read -p "Enter email : " email
+    echo "Using email $email"
+    if [ ! -f ~/.ssh/id_rsa ]; then
+        ssh-keygen -t rsa -b 4096 -C "$email"
+        ssh-add ~/.ssh/id_rsa
+    fi
+    pub=`cat ~/.ssh/id_rsa.pub`
+    # read -p "Enter github username: " githubuser
+    # echo "Using username $githubuser"
+    # read -s -p "Enter github password for user $githubuser: " githubpass
+    # echo
+    # read -p "Enter github OTP: " otp
+    # echo "Using otp $otp"
+    # echo
+    # curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" --header "x-github-otp: $otp" https://api.github.com/user/keys
+    echo "Unable to send to github so please add your SSH key to github"
+else
+    echo "You already have ssh keys"
+fi
+
+
+##############################################
+# Verify installation somehow?               #
+##############################################
